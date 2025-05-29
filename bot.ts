e437651b4b8169f1da27ecb98956391bad7c2721
Joshua W Julian <joshuawjulian@gmail.com>
2025-05-29 14:05:45 -0400

    upgrades

--- bot.ts
+++ bot.ts
@@ -1,39 +1,12 @@
-let DISCORD_TOKEN = Deno.env.get('DISCORD_TOKEN');
-console.log('TOKEN', DISCORD_TOKEN);
-if (DISCORD_TOKEN?.at(0) === '"') DISCORD_TOKEN = DISCORD_TOKEN.slice(1);
-if (DISCORD_TOKEN?.at(-1) === '"') DISCORD_TOKEN = DISCORD_TOKEN.slice(0, -1);
-
+import 'jsr:@std/dotenv/load';
 import {
 	AttachmentBuilder,
 	Client,
 	Events,
 	GatewayIntentBits,
-	Guild,
+	Message,
 } from 'npm:discord.js';
-import { liteAdaptor } from 'npm:mathjax-full/js/adaptors/liteAdaptor.js';
-import { RegisterHTMLHandler } from 'npm:mathjax-full/js/handlers/html.js';
-import { TeX } from 'npm:mathjax-full/js/input/tex.js';
-import { AllPackages } from 'npm:mathjax-full/js/input/tex/AllPackages.js';
-import { mathjax } from 'npm:mathjax-full/js/mathjax.js';
-import { SVG } from 'npm:mathjax-full/js/output/svg.js';
-import { latexStringToPngBuffer } from './convert.ts';
-
-const adaptor = liteAdaptor();
-RegisterHTMLHandler(adaptor);
-
-const mathjax_document = mathjax.document('', {
-	InputJax: new TeX({ packages: AllPackages }),
-	OutputJax: new SVG({ fontCache: 'local', scale: 2 }),
-});
-
-const mathjax_options = {
-	containerWidth: 1280,
-};
-
-export function get_mathjax_svg(math: string): string {
-	const node = mathjax_document.convert(math, mathjax_options);
-	return adaptor.innerHTML(node);
-}
+import { extractLatex, latexStringToPngBuffer } from './convert.ts';
 
 // Create a new client instance
 const client = new Client({
@@ -49,54 +22,89 @@ const latexMap = new Map<string, string>();
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
-client.once(Events.ClientReady, (readyClient) => {
+client.on(Events.ClientReady, (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 
-	const _guild = readyClient.guilds.cache.get('1325157265266970709') as Guild;
+	const _guild = readyClient.guilds.cache.get('1325157265266970709');
+	if (!_guild) {
+		console.error('Guild not found!');
+		return;
+	}
+
+	readyClient.on(Events.MessageCreate, async (message) => {
+		if (message.author.bot) return;
+		const latexStr = extractLatex(message.content);
+		if (latexStr == null) return;
+
+		const file = new AttachmentBuilder(await latexStringToPngBuffer(latexStr), {
+			name: 'latex.png',
+		});
+
+		const reply = await message.reply({ files: [file] });
+		latexMap.set(message.id, reply.id);
+		console.log('LaTeX:', latexStr);
+	});
 
 	readyClient.on(Events.MessageUpdate, async (oldMessage, newMessage) => {
-		const prevReply = latexMap.get(oldMessage.id);
-		if (prevReply) {
-			const replyChannel = await readyClient.channels.fetch(
-				oldMessage.channelId,
-			);
-			if (replyChannel && replyChannel.isTextBased()) {
-				const reply = await replyChannel.messages.fetch(prevReply);
-				if (reply) {
-					await reply.delete();
-					latexMap.delete(oldMessage.id);
+		if (oldMessage === null) return;
+		if (newMessage === null) return;
+		if (oldMessage.author && oldMessage.author.bot) return;
+		if (oldMessage.content === newMessage.content) return;
+
+		const oldLatex = extractLatex(oldMessage.content);
+		const newLatex = extractLatex(newMessage.content);
+
+		if (oldLatex == null && newLatex == null) return;
+		if (oldLatex === newLatex) return;
+
+		let reply: Message | null = null;
+
+		if (oldLatex) {
+			const prevReply = latexMap.get(oldMessage.id);
+			if (prevReply) {
+				const replyChannel = await readyClient.channels.fetch(
+					oldMessage.channelId,
+				);
+				if (replyChannel && replyChannel.isTextBased()) {
+					reply = await replyChannel.messages.fetch(prevReply);
 				}
 			}
 		}
 
-		if (newMessage.content.startsWith('!math ')) {
-			const latexString = newMessage.content.slice(6);
+		if (newLatex) {
 			const file = new AttachmentBuilder(
-				await latexStringToPngBuffer(latexString),
-				{ name: 'latex.png' },
+				await latexStringToPngBuffer(newLatex),
+				{
+					name: 'latex.png',
+				},
 			);
-			const reply = await newMessage.reply({ files: [file] });
-			latexMap.set(newMessage.id, reply.id);
-			console.log('LaTeX:', latexString);
-		}
-	});
 
-	readyClient.on(Events.MessageCreate, async (message) => {
-		if (message.content.startsWith('!math ')) {
-			const latexString = message.content.slice(6);
-			const file = new AttachmentBuilder(
-				await latexStringToPngBuffer(latexString),
-				{ name: 'latex.png' },
-			);
-			const reply = await message.reply({ files: [file] });
-			latexMap.set(message.id, reply.id);
-			console.log('LaTeX:', latexString);
+			if (reply) {
+				reply.edit({ files: [file] });
+			} else {
+				const newReply = await newMessage.reply({ files: [file] });
+				latexMap.set(newMessage.id, newReply.id);
+			}
+			console.log('LaTeX:', newLatex);
+		} else {
+			if (reply) {
+				await reply.delete();
+				latexMap.delete(oldMessage.id);
+			}
 		}
 	});
 });
-// Log in to Discord with your client's token
+
+let DISCORD_TOKEN = Deno.env.get('DISCORD_TOKEN');
+console.log('TOKEN', DISCORD_TOKEN);
+if (DISCORD_TOKEN?.at(0) === '"') DISCORD_TOKEN = DISCORD_TOKEN.slice(1);
+if (DISCORD_TOKEN?.at(-1) === '"') DISCORD_TOKEN = DISCORD_TOKEN.slice(0, -1);
+if (DISCORD_TOKEN === undefined) {
+	console.error('DISCORD_TOKEN is not set');
+	Deno.exit(1);
+}
 try {
-	client.login(DISCORD_TOKEN);
+	await client.login(DISCORD_TOKEN);
 } catch (error) {
 	console.error(`Failed to login with token: ${DISCORD_TOKEN}`);
 	console.error(error);
--- convert.ts
+++ convert.ts
@@ -21,6 +21,7 @@ const mathjax_options = {
 
 export function get_mathjax_svg(math: string): string {
 	const node = mathjax_document.convert(math, mathjax_options);
+	console.log(node);
 	return adaptor.innerHTML(node);
 }
 
@@ -29,4 +30,16 @@ export function latexStringToPngBuffer(latexString: string): Promise<Buffer> {
 	return sharp(new TextEncoder().encode(svg), { density: 250 })
 		.png()
 		.toBuffer();
+}
+
+export function extractLatex(content: string | null): string | null {
+	if (content === null) return null;
+	// Match $$...$$ or $...$
+	const latexRegex = /\$\$(.*?)\$\$|\$(.*?)\$/gs;
+	const matches = [...content.matchAll(latexRegex)];
+
+	if (matches.length === 0) return null;
+
+	// !TODO: Match multiple
+	return matches[0][1] || matches[0][2];
 }
